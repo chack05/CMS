8ab6306771b1a8c33482eb54ac69b37d94cbdf44
Chack05 <aduc4453@gmail.com>
2025-09-26 11:52:47 +0700

    hoan thanh

--- wordpress/wp-content/themes/twentytwenty/template-parts/footer-menus-widgets.php
+++ wordpress/wp-content/themes/twentytwenty/template-parts/footer-menus-widgets.php
@@ -114,6 +114,13 @@ if ( $has_footer_menu || $has_social_menu || $has_sidebar_1 || $has_sidebar_2 ) 
                             </div>
 
                         <?php } ?>
+                        <?php if ( $has_sidebar_4 ) { ?>
+
+                            <div class="footer-widgets column-two grid-item">
+                                <?php dynamic_sidebar( 'sidebar-4' ); ?>
+                            </div>
+
+                        <?php } ?>
 
 					</div><!-- .footer-widgets-wrapper -->
 
